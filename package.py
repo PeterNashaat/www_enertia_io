aaa43500d36450745f9b717999b72020d002d5b1
Bishoy <abdelmab@codescalers.com>
2019-12-08 18:28:52 +0200

    add http/s

--- package.py
+++ package.py
@@ -12,17 +12,18 @@ class Package(j.baseclasses.threebot_package):
         self.enertia_io = "https://github.com/Pishoy/www_enertia_io"
 
     def prepare(self):
-        website = self.openresty.get_from_port(443)
-        locations = website.locations.get("enertia")
-        static_location = locations.locations_static.new()
-        static_location.name = "enertia_static"
-        static_location.path_url = "/enertia"
-        path = j.clients.git.getContentPathFromURLorPath(self.enertia_io, branch=self.branch, pull=True)
-        html_path = "{0}/html".format(path)
-        static_location.path_location = html_path
-        static_location.use_jumpscale_weblibs = True # if set, will copy weblibs and serve it from /static/weblibs directly
-        locations.configure()
-        website.configure()
+        for port in (443, 80):
+            website = self.openresty.get_from_port(port)
+            locations = website.locations.get("enertia")
+            static_location = locations.locations_static.new()
+            static_location.name = "enertia_static"
+            static_location.path_url = "/enertia"
+            path = j.clients.git.getContentPathFromURLorPath(self.enertia_io, branch=self.branch, pull=True)
+            html_path = "{0}/html".format(path)
+            static_location.path_location = html_path
+            static_location.use_jumpscale_weblibs = True # if set, will copy weblibs and serve it from /static/weblibs directly
+            locations.configure()
+            website.configure()
 
     def start(self):
         self.prepare()
