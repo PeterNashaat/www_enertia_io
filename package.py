6cbb0c60aaeb1c3c2f36b9b0b2c59a7f4febb42b
PeterNashaat <peter@codescalers.com>
2019-12-30 15:47:03 +0200

    Update package.py

--- package.py
+++ package.py
@@ -1,4 +1,20 @@
 from Jumpscale import j
 
 class Package(j.baseclasses.threebot_package):
-    pass
+
+    def start(self):
+        server = self.openresty
+        server.configure()
+        for port in [80, 443]:
+            website = server.get_from_port(port)
+            locations = website.locations.get(f"enertia_locations_{port}")
+
+            website_location = locations.locations_static.new()
+            website_location.name = "enertiawebsite"
+            website_location.path_url = "/"
+            fullpath = j.sal.fs.joinPaths(self.package_root, "html/")
+            website_location.path_location = fullpath
+
+            locations.configure()
+            website.configure()
+            website.save()
